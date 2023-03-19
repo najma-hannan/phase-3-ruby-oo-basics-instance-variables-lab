2d47b318398df178cd36363e75fbb606b65bff6b
Hannan <najma.mahat@student.moringaschool.com>
2023-03-19 08:32:04 +0300

    Completed lab on instance variables code along

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
+# Setter -> Sets the name attribute for the Dog Object
+# assign the @name instance variable a value of whatever is passed as an argum
+def name=(value)
+    @this_dogs_name = value
 end
+
+# Getter -> Gets the name attribute of the Dog Object
+# access the data from the @name instance variable and return it
+def name
+    @this_dogs_name
+end
+
+end 
